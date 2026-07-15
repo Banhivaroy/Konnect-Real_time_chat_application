075e981f76c1bb7c1eeff7071ee8487d937598c2
Banhivaroy <roybanhiva@gmail.com>
2026-07-15 10:06:18 +0530

    main.jsx changed for googleOAuth

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,16 @@
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import App from "./App.jsx";
+import React from "react";
+import { GoogleOAuthProvider} from "@react-oauth/google"
 
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import './index.css'
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-)
+createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <GoogleOAuthProvider clientId = {import.meta.env.VITE_GOOGLE_CLIENT_ID}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </GoogleOAuthProvider>
+  </React.StrictMode>,
+);
