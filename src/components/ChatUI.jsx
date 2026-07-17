c91e5be3f6e762391daf304db5e5495b5732be45
Banhivaroy <roybanhiva@gmail.com>
2026-07-17 20:52:10 +0530

    chat ui made

--- src/components/ChatUI.jsx
+++ src/components/ChatUI.jsx
@@ -1,11 +1,53 @@
-import React from 'react'
-import SplineBackground from './SplineBackground'
+import React from "react";
+// import SplineBackground from "./SplineBackground";
+import Spline from '@splinetool/react-spline'
 function ChatUI() {
   return (
-    <div>
-      <SplineBackground/>
+    <div className="chat-page">
+      <aside className="sidebar">
+        <div className="sidebar-header">
+          <h2>Konnect</h2>
+          <button className="new-chat">+</button>
+        </div>
+
+        <div className="search-box">
+          <input placeholder="Search people..." />
+        </div>
+
+        <div className="chat-list">
+          <div className="chat-item active">
+            <img src="/avatar.png" />
+            <div>
+              <h4>Agniva Roy</h4>
+              <p>Typing...</p>
+            </div>
+          </div>
+
+          <div className="chat-item">
+            <img src="/avatar.png" />
+            <div>
+              <h4>Harry Potter</h4>
+              <p>Hello!</p>
+            </div>
+          </div>
+
+          <div className="chat-item">
+            <img src="/avatar.png" />
+            <div>
+              <h4>Ron Weasley</h4>
+              <p>See you soon.</p>
+            </div>
+          </div>
+        </div>
+      </aside>
+
+      <main className="chat-window">
+        <div className="bmo-container">
+          <Spline scene="https://prod.spline.design/Wjk7LoWwtAwDLfB6/scene.splinecode" />
+        </div>
+      </main>
     </div>
-  )
+  );
 }
 
-export default ChatUI
+export default ChatUI;
