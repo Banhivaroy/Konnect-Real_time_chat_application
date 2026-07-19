d8b3742617a49eb5741353e3a58a51af384e1ec4
Banhivaroy <roybanhiva@gmail.com>
2026-07-19 19:20:14 +0530

    fixed 2

---
+++ src/assets/floating-icons/export.js
@@ -0,0 +1,77 @@
+import antenna from "./antenna.svg";
+import battery1 from "./battery-1.svg";
+import battery2 from "./battery-2.svg";
+import battery3 from "./battery-3.svg";
+import bluetooth1 from "./bluetooth-1.svg";
+import bluetooth2 from "./bluetooth-2.svg";
+import burner from "./burner.svg";
+import calender16 from "./calender16.svg";
+import calender17 from "./calender17.svg";
+import calender18 from "./calender18.svg";
+import car from "./car.svg";
+import cloud from "./cloud.svg";
+import error from "./error.svg";
+import finger from "./finger.svg";
+import glass from "./glass.svg";
+import globes from "./globes.svg";
+import house from "./house.svg";
+import lamp from "./lamp.svg";
+import lifelink from "./lifelink.svg";
+import link from "./link.svg";
+import mic from "./mic.svg";
+import mic2 from "./mic2.svg";
+import mobile from "./mobile.svg";
+import plantmoney from "./plantmoney.svg";
+import printer from "./printer.svg";
+import renew from "./renew-team-svgrepo-com.svg";
+import satelite from "./satelite.svg";
+import scan from "./scan.svg";
+import telephone from "./telephone.svg";
+import turbine from "./turbine.svg";
+import tv from "./tv.svg";
+import tv2 from "./tv2.svg";
+import walkie from "./walkie.svg";
+import watch from "./watch.svg";
+import watch2 from "./watch2.svg";
+import wave from "./wave.svg";
+
+const icons = [
+  antenna,
+  battery1,
+  battery2,
+  battery3,
+  bluetooth1,
+  bluetooth2,
+  burner,
+  calender16,
+  calender17,
+  calender18,
+  car,
+  cloud,
+  error,
+  finger,
+  glass,
+  globes,
+  house,
+  lamp,
+  lifelink,
+  link,
+  mic,
+  mic2,
+  mobile,
+  plantmoney,
+  printer,
+  renew,
+  satelite,
+  scan,
+  telephone,
+  turbine,
+  tv,
+  tv2,
+  walkie,
+  watch,
+  watch2,
+  wave,
+];
+
+export default icons;
